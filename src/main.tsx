a4a869d1e66b24bc9ef8dac84e9f5c3aa82598bb
Gorbachev Egor <7gorbachevm@gmail.com>
2022-07-13 19:27:24 +0300

    Init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+      <App />
+      {/*<div className={'guide__component-wrapper'}>*/}
+      {/*    <div className={'infinite-cards__guide-wrapper'}>*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+  </React.StrictMode>
+)
