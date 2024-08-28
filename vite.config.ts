26e83b96a40de2619bec0b6559a1e0e9a4ec8881
Gorbachev Egor <7gorbachevm@gmail.com>
2024-08-28 20:29:49 +0700

    Test theme_color

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      name: 'Alias game',
+      theme_color: '#f00',
+    }
+  })],
   base: '/alias/',
 });
