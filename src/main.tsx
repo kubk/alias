84388836c7a24794dbcbc6df011bb483b6a8a003
Gorbachev Egor <7gorbachevm@gmail.com>
2024-08-28 19:53:27 +0700

    Trigger deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Page } from "./page";
 
+// depl
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Page />
 );
