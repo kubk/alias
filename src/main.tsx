76914fc8f384d11f6ad221c91df6e02c2ba3a7c3
Gorbachev Egor <7gorbachevm@gmail.com>
2024-08-28 20:15:49 +0700

    Optimize PWA for iOS

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Page } from "./page";
 
-// depl
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Page />
 );
