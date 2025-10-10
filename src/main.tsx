fd462058c530f0a91372ad349e9a5a03e2c2e3f6
Gorbachev Egor <7gorbachevm@gmail.com>
2025-10-10 13:51:58 +0700

    Revert "Remove PWA manifest and unregister service workers"
    This reverts commit 6f22bdded7d1a5084e1f36206f33c90b449d3269.

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Page } from "./page";
 
-// Unregister any existing service workers
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (const registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Page />
 );
